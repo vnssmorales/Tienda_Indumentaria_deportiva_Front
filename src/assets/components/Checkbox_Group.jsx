cb1f197794ac7e81fb3c9ffcb699842e47d239ab
mjtBootcamp <115650100+mjtBootcamp@users.noreply.github.com>
2023-07-31 16:41:38 -0400

    Catogias nuevas

--- src/assets/components/Checkbox_Group.jsx
+++ src/assets/components/Checkbox_Group.jsx
@@ -3,10 +3,10 @@ import { useRef } from "react";
 function Checkbox_Group(props) {
     
     let inputRef = useRef()
-    let inputMenClothing = useRef()
-    let inputJewelery = useRef()
-    let inputElectronics = useRef()
-    let inputWomenClothing = useRef()
+    let inputZapatillas = useRef()
+    let inputShorts = useRef()
+    let inputCalzas = useRef()
+    let inputPoleras = useRef()
    
     return (
         <>
@@ -15,34 +15,34 @@ function Checkbox_Group(props) {
                     <div className="input-group">
                         <div className="input-group-prepend">
                             <div className="input-group-text">
-                                <input type="checkbox" value="men's clothing" ref={inputMenClothing} onChange={() => props.filterProducts()} />
+                                <input type="checkbox" value="zapatillas" ref={inputZapatillas} onChange={() => props.filterProducts()} />
                             </div>
                         </div>
-                        <p className="mb-0 px-1 pt-1 pt-1">MenClothing</p>
+                        <p className="mb-0 px-1 pt-1 pt-1">Zapatillas</p>
                     </div>
                     <div className="input-group">
                         <div className="input-group-prepend">
                             <div className="input-group-text">
-                                <input type="checkbox" value="jewelery" ref={inputJewelery} onChange={() => props.filterProducts()} />
+                                <input type="checkbox" value="shorts" ref={inputShorts} onChange={() => props.filterProducts()} />
                             </div>
                         </div>
-                        <p className="mb-0 px-1 pt-1 pt-1">Jewelery</p>
+                        <p className="mb-0 px-1 pt-1 pt-1">Shorts</p>
                     </div>
                     <div className="input-group">
                         <div className="input-group-prepend">
                             <div className="input-group-text">
-                                <input type="checkbox" value="electronics" ref={inputElectronics} onChange={() => props.filterProducts()} />
+                                <input type="checkbox" value="calzas" ref={inputCalzas} onChange={() => props.filterProducts()} />
                             </div>
                         </div>
-                        <p className="mb-0 px-1 pt-1 pt-1">Electronics</p>
+                        <p className="mb-0 px-1 pt-1 pt-1">Calzas</p>
                     </div>
                     <div className="input-group">
                         <div className="input-group-prepend">
                             <div className="input-group-text">
-                                <input type="checkbox" value="women's clothing" ref={inputWomenClothing} onChange={() => props.filterProducts()} />
+                                <input type="checkbox" value="poleras" ref={inputPoleras} onChange={() => props.filterProducts()} />
                             </div>
                         </div>
-                        <p className="mb-0 px-1 pt-1 pt-1">WomenClothing</p>
+                        <p className="mb-0 px-1 pt-1 pt-1">Poleras</p>
                     </div>
                     <div className="input-group">
                         <input type="text" className="form-control" id="textsearch" ref={inputRef} />
--- src/assets/store/StateProvider.jsx
+++ src/assets/store/StateProvider.jsx
@@ -1,4 +1,4 @@
-import { useState , useEffect } from "react";
+import { useState, useEffect } from "react";
 import StateContent from "./StateContext"
 
 import axios from 'axios'
@@ -7,9 +7,18 @@ import ProductsContext from "./StateContext";
 
 
 const ProductProvider = ({ children }) => {
-    const URL = "https://fakestoreapi.com/products"
+    //const URL = "https://fakestoreapi.com/products"
+    const URL = "http://localhost:3100/api/productos"
     const [products, setProducts] = useState([])
     useEffect(() => {
+
+        /* axios.get(URL)
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.log('Error', error);
+            }); */
         const getProducts = async () => {
             try {
                 const response = await axios.get(URL)
