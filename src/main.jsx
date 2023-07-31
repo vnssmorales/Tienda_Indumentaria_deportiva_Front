ab176e58c454f39233c6b13aaa5b99517051657b
vanemh <vnss.morales@gmail.com>
2023-07-31 15:43:39 -0400

    context para usuarios

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import ProductProvider from './store/StateProvider.jsx'
+import { Auth } from 'aws-amplify'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AuthProvider>
     <ProductProvider>
     <App />
     </ProductProvider>
+    </AuthProvider>
   </React.StrictMode>,
 )
---
+++ src/profileContext/AuthProvider.jsx
@@ -0,0 +1,13 @@
+import AuthContext from "./AuthContext";
+
+const AuthProvider = ({ children }) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    return (
+        <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+            {children}
+        </AuthContext.Provider>
+    )
+
+};
+export default AuthProvider;
